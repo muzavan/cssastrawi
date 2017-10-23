6b80c17a115f70d970d9bcfe2fdad500446ca46e
Reza <reza.irvanda@xtremax.com>
2017-10-23 14:42:33 +0700

    Porting prefixrules1* done

---
+++ CSSastrawi.Source/morphology/defaultimpl/visitor/prefixrules/PrefixRule13a.cs
@@ -0,0 +1,50 @@
+
+using System.Text.RegularExpressions;
+/**
+* CSSastrawi is licensed under The MIT License (MIT)
+*
+* Copyright (c) 2017 Muhammad Reza Irvanda
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+namespace CSSastrawi.Morphology.Defaultimpl.Visitor.Prefixrules
+{
+    /**
+     * Disambiguate Prefix Rule 13a : mem{rV|V} -&gt; me-m{rV|V}
+     */
+    public class PrefixRule13a : Disambiguator
+    {
+
+        const string prefixRule = "^mem([aiueo])(.*)$";
+        public string Disambiguate(string word)
+        {
+            var rule = new Regex(prefixRule, RegexOptions.Compiled);
+            var match = rule.Match(word);
+            if (match.Success)
+            {
+                var groups = match.Groups;
+                return "m" + groups[0].Value + groups[1].Value;
+            }
+
+            return word;
+        }
+    }
+
+}
