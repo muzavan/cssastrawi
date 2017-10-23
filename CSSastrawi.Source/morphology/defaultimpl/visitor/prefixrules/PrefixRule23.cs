6cda8bab1e4f41ec8b0a083ab2cc8bd4312f0e85
Reza <reza.irvanda@xtremax.com>
2017-10-23 15:05:17 +0700

    Porting PrefixRule2 done

---
+++ CSSastrawi.Source/morphology/defaultimpl/visitor/prefixrules/PrefixRule23.cs
@@ -0,0 +1,56 @@
+
+using System.Text.RegularExpressions;
+/**
+* CSSastrawi is licensed under The MIT License (MIT)
+*
+* Copyright (c) 2017 Muhammad Reza Irvanda
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+namespace CSSastrawi.Morphology.Defaultimpl.Visitor.Prefixrules
+{
+
+    /**
+     * Disambiguate Prefix Rule 23 : perCAP -&gt; per-CAP where C != 'r' AND P !=
+     * 'er'
+     */
+    public class PrefixRule23 : Disambiguator
+    {
+        const string prefixRule = "^per([bcdfghjklmnpqrstvwxyz])([a-z])(.*)$";
+        public string Disambiguate(string word)
+        {
+            var rule = new Regex(prefixRule, RegexOptions.Compiled);
+            var match = rule.Match(word);
+            if (match.Success)
+            {
+                var groups = match.Groups;
+                var nextRule = new Regex("^er(.*)$");
+                if (nextRule.Match(groups[2].Value).Success)
+                {
+                    return word;
+                }
+                return groups[0].Value + groups[1].Value + groups[2].Value;
+            }
+
+            return word;
+        }
+    }
+
+}
