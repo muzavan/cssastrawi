450b47261335b02e4afa68cb21e11d3f350b6630
Reza <reza.irvanda@xtremax.com>
2017-10-23 09:53:04 +0700

    Porting for cli.output down

---
+++ CSSastrawi.Source/cli/output/BufferedOutput.cs
@@ -0,0 +1,58 @@
+
+
+using System.Text;
+/**
+* JSastrawi is licensed under The MIT License (MIT)
+*
+* Copyright (c) 2017 Muhammad Reza Irvanda
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+namespace CSSastrawi.Cli.Output
+{
+    /// <summary>
+    /// An implementation of Output with buffering using StringBuilder.
+    /// </summary>
+    public class BufferedOutput : Output
+    {
+        private readonly StringBuilder buffer = new StringBuilder();
+        
+        public void WriteLine(string x)
+        {
+            buffer.AppendLine(x);
+        }
+        
+        public void Write(string x)
+        {
+            buffer.Append(x);
+        }
+        
+        /// <summary>
+        /// Get all buffered string
+        /// </summary>
+        /// <returns>buffered string</returns>
+        public override string ToString()
+        {
+            return buffer.ToString();
+        }
+    }
+}
+
+
